b55f9ceef250bb8fd738478fc8d925083dc632bf
Rohit R M <163559461+Rohit-R-M@users.noreply.github.com>
2024-08-03 20:31:20 +0530

    Update pass 2.c

--- pass 2.c
+++ pass 2.c
@@ -1,136 +1,56 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 int main() 
 {
-    char label[10], opcode[10], operand[10], code[10], mnemonic[10];
-    char symbol[10], address[10];
-    int locctr, start, length;
-    int textLength = 0;
-    char textRecord[70] = "";
-
-    FILE *intermediate, *optab, *symtbl, *output, *len;
-
-    intermediate = fopen("out.txt", "r");
-    optab = fopen("optab.txt", "r");
-    symtbl = fopen("symtab.txt", "r");
-    output = fopen("output.txt", "w");
-    len = fopen("length.txt", "r");
-
-    if (intermediate == NULL || optab == NULL || symtbl == NULL || output == NULL || len == NULL)
-	{
-        perror("Error opening files");
-        return 1;
+    char label[10], opcode[10], operand[10], symbol[10], code[10], mnemonic[10];
+    int locctr, start, length, address;
+    FILE *fp1, *fp2, *fp3, *fp4, *fp5;
+    fp1 = fopen("intermediate.txt", "r");
+    fp2 = fopen("optab.txt", "r");
+    fp3 = fopen("symtbl.txt", "r");
+    fp4 = fopen("output1.txt", "w");
+    fp5 = fopen("length.txt", "r");
+    fscanf(fp5, "%d", &length);
+    fclose(fp5);
+    fscanf(fp1, "%s\t%s\t%s", label, opcode, operand);
+    if(strcmp(opcode, "START") == 0) {
+        start = atoi(operand);
+        locctr = start;
+        fprintf(fp4, "H^%s^%06d^%06d\n", label, start, length);
+        fscanf(fp1, "%s\t%s\t%s", label, opcode, operand);
     }
-
-    fscanf(len, "%d", &length);
-    fscanf(intermediate, "%X\t%s\t%s\t%s", &locctr, label, opcode, operand);
-
-    if (strcmp(opcode, "START") == 0) 
-	{
-        start = locctr;
-        fprintf(output, "H^%s^%06X^%06X\n", label, start, length);
-        fscanf(intermediate, "%X\t%s\t%s\t%s", &locctr, label, opcode, operand);
-    }
-
-    int textStart = locctr;
-
-    while (strcmp(opcode, "END") != 0) 
-	{
-        if (strcmp(opcode, "BYTE") == 0) 
-		{
-            if (operand[0] == 'C') 
-			{
-                for (int i = 2; operand[i] != '\''; i++) 
-				{
-                    char hex[3];
-                    sprintf(hex, "%02X", operand[i]);
-                    strcat(textRecord, hex);
-                    textLength++;
-                }
-            } 
-			else if (operand[0] == 'X') 
-			{
-                for (int i = 2; operand[i] != '\''; i++) 
-				{
-                    char hex[2];
-                    sprintf(hex, "%c", operand[i]);
-                    strcat(textRecord, hex);
-                }
-                textLength += (strlen(operand) - 3) / 2;
+    fprintf(fp4, "T^%06d^", locctr);
+    while(strcmp(opcode, "END") != 0) {
+        if(strcmp(opcode, "BYTE") == 0 || strcmp(opcode, "WORD") == 0) {
+            if(strcmp(opcode, "BYTE") == 0) {
+                fprintf(fp4, "^%s", operand);
+            } else {
+                fprintf(fp4, "^%06d", atoi(operand));
             }
-        }
-		else if (strcmp(opcode, "WORD") == 0) 
-		{
-            char temp[7];
-            sprintf(temp, "%06X", atoi(operand));
-            strcat(textRecord, temp);
-            textLength += 3;
-        } 
-		else if (strcmp(opcode, "RESW") == 0 || strcmp(opcode, "RESB") == 0) 
-		{
-            if (textLength > 0) 
-			{
-                fprintf(output, "T^%06X^%02X^%s\n", textStart, textLength, textRecord);
-                textLength = 0;
-                textRecord[0] = '\0';
-            }
-            textStart = locctr + (strcmp(opcode, "RESW") == 0 ? 3 * atoi(operand) : atoi(operand));
-        } 
-		else 
-		{
-            rewind(optab);
-            fscanf(optab, "%s\t%s", code, mnemonic);
-            while (strcmp(code, "END") != 0) 
-			{
-                if (strcmp(opcode, code) == 0) 
-				{
-                    strcat(textRecord, mnemonic);
-                    textLength += 3;
-
-                    rewind(symtbl);
-                    fscanf(symtbl, "%s\t%s", symbol, address);
-                    while (strcmp(symbol, operand) != 0 && !feof(symtbl)) 
-					{
-                        fscanf(symtbl, "%s\t%s", symbol, address);
+        } else {
+            rewind(fp2);
+            fscanf(fp2, "%s\t%s", code, mnemonic);
+            while(strcmp(code, "END") != 0) {
+                if(strcmp(opcode, code) == 0) {
+                    rewind(fp3);
+                    fscanf(fp3, "%s\t%d", symbol, &address);
+                    while(strcmp(symbol, operand) != 0) {
+                        fscanf(fp3, "%s\t%d", symbol, &address);
                     }
-                    if (strcmp(symbol, operand) == 0) 
-					{
-                        strcat(textRecord, address);
-                    } else 
-					{
-                        strcat(textRecord, "0000");
-                    }
+                    fprintf(fp4, "^%s%06d", mnemonic, address);
                     break;
                 }
-                fscanf(optab, "%s\t%s", code, mnemonic);
+                fscanf(fp2, "%s\t%s", code, mnemonic);
             }
         }
-
-        fscanf(intermediate, "%X\t%s\t%s\t%s", &locctr, label, opcode, operand);
-
-        if (textLength >= 30) 
-		{
-            fprintf(output, "T^%06X^%02X^%s\n", textStart, textLength, textRecord);
-            textLength = 0;
-            textRecord[0] = '\0';
-            textStart = locctr;
-        }
-    } 
-    if (textLength > 0) 
-	{
-        fprintf(output, "T^%06X^%02X^%s\n", textStart, textLength, textRecord);
+        fscanf(fp1, "%s\t%s\t%s", label, opcode, operand);
     }
-    fprintf(output, "E^%06X\n", start);
 
-    fclose(intermediate);
-    fclose(optab);
-    fclose(symtbl);
-    fclose(output);
-    fclose(len);
-
-    printf("Pass 2 Assembler: Object code generated in output.txt\n");
+    fprintf(fp4, "\nE^%06d\n", start);
+    fclose(fp1);
+    fclose(fp2);
+    fclose(fp3);
+    fclose(fp4);
     return 0;
 }
-
